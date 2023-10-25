9ae3492ad5238e07736210a7d458f67de696d1fb
swiftcode <swiftcode@users.noreply.github.com>
2023-10-25 11:47:01 -0500

    Initial Commit

---
+++ WorldSwiftData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldSwiftData
+//
+//  Created by Michael Campbell on 10/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
